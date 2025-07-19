f78ab24d353bb7d6fe6834ad3629dcd2ecd8216e
sivasatyasaik <koururavi999@gmail.com>
2025-07-19 22:26:15 +0530

    Update 4 index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,28 @@
-from main import app  # absolute import, no dot
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+import os
+import yt_dlp
 
-handler = app
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # restrict in production!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+cur_dir = os.getcwd()
+
+@app.post("/api/download")
+async def download_video(link: str = Form(...)):
+    # This will try to download inside serverless environment
+    # may not work for big files or long tasks
+    youtube_dl_options = {
+        "format": "best",
+        "outtmpl": os.path.join(cur_dir, f"video-{link[-11:]}.mp4")
+    }
+    with yt_dlp.YoutubeDL(youtube_dl_options) as ydl:
+        ydl.download([link])
+    return {"status": "Download started"}
